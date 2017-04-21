5268988790c09dff5dd2f5cf769a6e36c795db27
Jean Carvajal <jecarvaj@alumnos.ubiobio.cl>
2017-04-21 05:29:59 -0300

    Agrego activities e intents

--- app/src/main/java/com/example/jean/collectbeer/MainActivity.java
+++ app/src/main/java/com/example/jean/collectbeer/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.jean.collectbeer;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+RelativeLayout nuevo, consultar, mostrarTodo, configuracion;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nuevo =(RelativeLayout) findViewById(R.id.layoutNuevo);
+        consultar=(RelativeLayout) findViewById(R.id.layoutConsultar);
+        mostrarTodo=(RelativeLayout) findViewById(R.id.layoutMostrar);
+        configuracion=(RelativeLayout) findViewById(R.id.layoutConfig);
+
+        nuevo.setOnClickListener(this);
+        consultar.setOnClickListener(this);
+        mostrarTodo.setOnClickListener(this);
+        configuracion.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.layoutNuevo:
+                intent=new Intent(this, NuevoActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.layoutConsultar:
+                intent=new Intent(this, ConsultaActivity.class);
+                startActivity(intent);;
+                break;
+            case R.id.layoutMostrar:
+                intent=new Intent(this, MostrarActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.layoutConfig:
+                intent=new Intent(this, ConfigActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+
+
 }
